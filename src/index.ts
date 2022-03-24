091cc8281bfc99b9d77623951fc187ffe2c556b0
Robert Soriano <sorianorobertc@gmail.com>
2022-03-24 13:24:45 -0700

    init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-export const foo = 'foo'
+import type { Options as TsupOptions } from 'tsup'
+import type { SolidOptions } from 'esbuild-plugin-solid'
+import { solidPlugin } from 'esbuild-plugin-solid'
+
+type PluginArr = TsupOptions['plugins']
+
+export default function SolidPlugin(options?: SolidOptions) {
+  const tmp: PluginArr = [{
+    name: 'tsup-plugin-solid',
+    esbuildOptions(ctx) {
+      ctx.plugins?.push(solidPlugin(options))
+    },
+  }]
+
+  return tmp[0]
+}
